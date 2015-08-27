bb1f1d386a2f3ada0d20aecae18c7cb6901a59ad
kakakaya <kakakaya+git@gmail.com>
2015-08-28 03:22:10 +0900

    気になっていた部分を修正、及びPython3化

--- tw-notifyd.py
+++ tw-notifyd.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 # Author: kakakaya, Date: Thu Feb 26 05:23:07 2015
 
@@ -11,22 +11,34 @@ import json
 import twitter as tw
 # import sqlite3
 import argparse
-import datetime
+from datetime import datetime, timedelta
 
 
-def loadConfig():
+def loadConfig(args):
     # メモ:
     # 強い監視 := 画像も保存、など
     # 弱い監視 := テキスト本文のみ
     # 無視 := 保存しない
     # {{強, 弱}い監視, 無視}をする対象の{アカウント、発言}を分類できるようにする
-    return {}
+    with open('key.json') as key:
+        keys = key.read()
+        conf = json.loads(keys)
+
+    # argsの内容に応じて上書きをする
+    if args.v:
+        conf["verbose"] = True
+
+    return conf
 
 
 def reformDate(created_at):
-    # Thu Mar 12 15:54:41 +0000 2015
-    time = datetime.datetime.strptime(created_at, "%a %b %d %H:%M:%S +0000 %Y")
-    return time.strftime("(%y/%m/%d_%H:%M:%S)")
+    u"""Thu Mar 12 15:54:41 +0000 2015
+    みたいな形式から
+    2015/03/13_00:54:41
+    みたいな形式に変える
+    """
+    time = datetime.strptime(created_at, "%a %b %d %H:%M:%S +0000 %Y") + timedelta(hours=9)
+    return time.strftime("%y/%m/%d_%H:%M:%S")
 
 
 def matchRule(rules, message):
@@ -36,34 +48,39 @@ def matchRule(rules, message):
         return False
 
 
+def hangupNotify():
+    time = datetime.now().strftime("%y/%m/%d_%H:%M:%S")
+    print("[HANGUP]", time)
+
+
 def deleteNotify(msg, db):
     delId = msg["delete"]["status"]["id"]
     tweets = db.tweets
-    tweets.create_index([("id", DESCENDING)])
     delList = list(tweets.find({"id": delId}))
     if len(delList) == 1:
         deletedMsg = delList[0]
         created_at = reformDate(deletedMsg["created_at"])
-        print "[DEL]"+created_at+" @"+deletedMsg["user"]["screen_name"]+": ", deletedMsg["text"]
+        print("[DEL]", created_at, "@"+deletedMsg["user"]["screen_name"]+": ", deletedMsg["text"])
         # notifyTweet("[DEL] @"+deletedMsg["user"]["screen_name"]+": "+deletedMsg["text"])
     else:
-        # print "[DEL] Unknown ID:", delId
+        # print("[DEL] Unknown ID:", delId)
         pass
 
 
 def favoriteNotify(msg):
     created_at = reformDate(msg["created_at"])
-    print "[FAV]"+created_at+" @"+msg["source"]["screen_name"]+" to @"+msg["target"]["screen_name"]
+    print("[FAV]"+created_at+" @"+msg["source"]["screen_name"]+" to @"+msg["target"]["screen_name"])
 
 
 def notifyTweet(tweet):
     pass
 
 
-def loop(auth, args):
+def loop(auth, conf):
     client = MongoClient()
     db = client["tw-notifyd_tweet"]
     tweets = db.tweets
+    tweets.create_index([("id", DESCENDING)])
     twitter_stream = tw.TwitterStream(
         auth=auth,
         domain="userstream.twitter.com"
@@ -71,46 +88,51 @@ def loop(auth, args):
     rules = loadConfig()
     rules["verbose"] = False
     # argsに基いてrulesの上書きを行う
-    if args.v:
+    if conf["verbose"]:
         rules["verbose"] = True
-    print "[INFO]Started loop", rules
-    for msg in twitter_stream.user():
-        # if matchRule(rules, msg):
-        #     # notifyTweet(rules, msg)
-        #     pprint(msg),
-        #     print len(msg)
-        #     pass
-        if "text" in msg.keys():
-            # seems to be a normal tweet
-            tweets.insert(msg)
-        elif "delete" in msg.keys():
-            # deleted tweet
-            deleteNotify(msg, db)
-            if rules["verbose"]:
-                pp(msg)
-        elif "event" in msg.keys():
-            event_type = msg["event"]
-            if event_type == "favorite":
-                favoriteNotify(msg)
+    print("[INFO]Started loop", rules)
+
+    try:
+        for msg in twitter_stream.user():
+            # if matchRule(rules, msg):
+            #     # notifyTweet(rules, msg)
+            #     pprint(msg),
+            #     print(len(msg))
+            #     pass
+            if "text" in msg.keys():
+                # seems to be a normal tweet
+                tweets.insert(msg)
+            elif "delete" in msg.keys():
+                # deleted tweet
+                deleteNotify(msg, db)
                 if rules["verbose"]:
                     pp(msg)
-            elif event_type == "favorited_retweet":
+            elif "event" in msg.keys():
+                event_type = msg["event"]
+                if event_type == "favorite":
+                    favoriteNotify(msg)
+                    if rules["verbose"]:
+                        pp(msg)
+                elif event_type == "favorited_retweet":
+                    pass
+                elif event_type == "retweeted_retweet":
+                    pass
+                elif event_type == "follow":
+                    pass
+                else:
+                    pp(msg)
+            elif "friends" in msg.keys():
                 pass
-            elif event_type == "retweetd_retweet":
-                pass
-            elif event_type == "follow":
-                pass
+            elif "hangup":
+                # 上に繋げてやりなおす
+                hangupNotify()
             else:
-                print "unknown event",
+                print "unknown type",
                 pp(msg)
-        elif "friends" in msg.keys():
-            pass
-        elif "hangup":
-            # 上に繋げてやりなおす
-            pass
-        else:
-            print "unknown type",
-            pp(msg)
+
+    except ValueError:
+        # JSONの読み取り失敗
+        pass
 
 
 def main():
@@ -118,23 +140,22 @@ def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-v', action='store_true')
+    parser.add_argument('--reset', action='store_true')
     args = parser.parse_args()
+    conf = loadConfig(args)     # 引数で設定を適当に上書く
 
-    with open('key.json') as key:
-        keys = key.read()
-    data = json.loads(keys)
     # pprint(data)
-    CONSUMER_KEY = data["consumerKey"]
-    CONSUMER_SECRET = data["consumerSecret"]
-    ACCESS_TOKEN = data["accessToken"]
-    ACCESS_TOKEN_SECRET = data["accessSecret"]
+    CONSUMER_KEY = conf["consumerKey"]
+    CONSUMER_SECRET = conf["consumerSecret"]
+    ACCESS_TOKEN = conf["accessToken"]
+    ACCESS_TOKEN_SECRET = conf["accessSecret"]
 
     while True:
         auth = tw.OAuth(
             ACCESS_TOKEN, ACCESS_TOKEN_SECRET,
             CONSUMER_KEY, CONSUMER_SECRET
         )
-        loop(auth, args)
+        loop(auth, conf)
 
 
 if __name__ == "__main__":
